2148ba428a652d80e9d3ff63d678b66a737f3063
Mayankrajput70 <88483601+Mayankrajput70@users.noreply.github.com>
2021-09-13 14:14:49 +0530

    Update EmployeeReadDto.scala

--- Question3/src/main/scala/com/knoldus/db/EmployeeReadDto.scala
+++ Question3/src/main/scala/com/knoldus/db/EmployeeReadDto.scala
@@ -5,10 +5,10 @@ import scala.collection.immutable.HashMap
 
 class EmployeeReadDto {
 
-  val ShashikantEmployee: Employee = Employee("Mayank","Rajput",22,12000,"Intern","knoldus","mayank.rajput@knoldus.com")
+  val MayankEmployee: Employee = Employee("Mayank","Rajput",22,12000,"Intern","knoldus","mayank.rajput@knoldus.com")
   val AmanEmployee: Employee = Employee("Aman","singh",24,15000,"Software Engineer","Philips","aman.singh@philips.com")
 
-  val employees: HashMap[String, Employee] = HashMap("Mayank" -> ShashikantEmployee, "Aman" -> AmanEmployee)
+  val employees: HashMap[String, Employee] = HashMap("Mayank" -> MayankEmployee, "Aman" -> AmanEmployee)
   def getEmployeeByName(name: String): Option[Employee] = employees.get(name)
 
 }
